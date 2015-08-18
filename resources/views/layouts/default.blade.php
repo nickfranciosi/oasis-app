c2464893e49d5096110176eb6cb49ea729a0184e
Nick Franciosi <nickfranciosi@gmail.com>
2015-08-18 13:36:22 -0500

    added view layout structure

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Oasis</title>
+</head>
+<body>
+
+    @yield('content')
+
+
+
+    @yield('scripts')
+    
+</body>
+</html>
