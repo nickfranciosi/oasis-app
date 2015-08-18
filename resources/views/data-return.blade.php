67d64cba89bd9c2bfef286bb8c4adf8926ad2908
Nick Franciosi <nickfranciosi@gmail.com>
2015-08-18 13:35:34 -0500

    added profile pic of logged in user

--- resources/views/data-return.blade.php
+++ resources/views/data-return.blade.php
@@ -1,11 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.default')
 
+
+@section('content')
+    <div>
+        <img src="https://graph.facebook.com/{{$user->facebook_user_id}}/picture??width=500&height=500">
+    </div>
     @foreach ($response['data'] as $image) 
 
         <img style="max-width: 150px;" src="{{$image['images'][0]['source']}}" >
@@ -13,5 +12,11 @@
     @endforeach
 
     <a href="{{$response['paging']['next']}}">Load More</a>
-</body>
-</html>
+
+@stop 
+
+
+@section('scripts')
+
+
+@stop
