a5de33dbcb7ca39223c20bbbbd31f1c6ff449bd9
Nick Franciosi <nickfranciosi@gmail.com>
2015-08-31 14:09:44 -0500

    only show share if logged in usre

--- resources/views/pages/story.blade.php
+++ resources/views/pages/story.blade.php
@@ -13,6 +13,9 @@
     @if ($user->facebook_user_id == Auth::user()->facebook_user_id)
      <p>You are logged in</p>
     @endif
+
+    {{ Auth::user()->facebook_user_id }}
+    {{ $user->facebook_user_id  }}
     <button id="fbTest">Share On Facebook</button>
     <a href="https://twitter.com/share" class="twitter-share-button" data-text="Oasis Awareness Campaign" data-via="oasis" data-size="large" data-count="none" data-hashtags="oasis">Tweet</a>
     <a href="/{{ $user->image_path }}" id="downloadLink" download>Download</a>
