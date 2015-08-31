939d871df5de675feb83040416ea2d73b7adb6bb
Nick Franciosi <nickfranciosi@gmail.com>
2015-08-31 13:12:50 -0500

    added image download - updated path

--- resources/views/pages/story.blade.php
+++ resources/views/pages/story.blade.php
@@ -15,7 +15,7 @@
     
     <button id="fbTest">Share On Facebook</button>
     <a href="https://twitter.com/share" class="twitter-share-button" data-text="Oasis Awareness Campaign" data-via="oasis" data-size="large" data-count="none" data-hashtags="oasis">Tweet</a>
-    <a href="//{{ $user->image_path }}" id="downloadLink">Download</a>
+    <a href="/{{ $user->image_path }}" id="downloadLink">Download</a>
     
 @stop
 
