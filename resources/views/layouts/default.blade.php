0701bb408eee87ec1bc3ddcd7abd09a15e7a25af
Nick Franciosi <nickfranciosi@gmail.com>
2015-08-31 08:51:56 -0500

    added dynamic image

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -9,7 +9,10 @@
    <meta property="og:description" content="This is the description of the awerness campaign.
    This needs to be at lest two sentences long and describe what the site is about." />
    <meta property="fb:app_id" content="1477990922522983" />
-   <meta property="og:image" content="/images/10100411290737202.png" />
+   @if ($user)
+    <meta property="og:image" content="/{{ $user->image_path }}" />
+   @endif
+   
     <title>Oasis</title>
 </head>
 <body>
