518ea1e46f5670b20ba97cb1d6a1a25eb06a3df2
Nick Franciosi <nickfranciosi@gmail.com>
2015-09-06 11:23:43 -0500

    each gallery image links to profile

--- resources/views/layouts/partials/section-instagram-grid.blade.php
+++ resources/views/layouts/partials/section-instagram-grid.blade.php
@@ -3,7 +3,7 @@
   <div class="container">
     <h2 class="header text-center">Instagram</h2>
     <div class="row"></div>@foreach ($allUsers as $currentUser)
-    <div class="col-sm-4"><img src="/{{ $currentUser['image_path'] }}" class="img-max"/></div>@endforeach
+    <div class="col-sm-4"><a href="/profile/{{$currentUser['facebook_user_id']}}"><img src="/{{ $currentUser['image_path'] }}" class="img-max"/></a></div>@endforeach
     <div class="view-more"><a class="btn-main veiw-more">View More</a></div>
   </div>
 </section>
