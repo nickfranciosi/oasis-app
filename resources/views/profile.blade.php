a70395314e1ada72c381297d75be62ff27995423
Nick Franciosi <nickfranciosi@gmail.com>
2015-09-06 11:56:30 -0500

    removed meta tag for image

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -19,6 +19,7 @@
     <meta name="twitter:image" content="http://placehold.it/1600x640">
     <!-- Opengraph-->
     <meta property="og:title" content="">
+    <meta property="og:type" content="website">
     <meta property="og:site_name" content="Oasis - Theres More to Youth">
     <meta property="og:description" content="Oasis Center #moretoyouth is here to help at-risk youths navigate tricky waters of the teenage years with a wide range of programs that are designed to empower them and make them aware of their potential.">
     <meta property="og:locale" content="en_US">
