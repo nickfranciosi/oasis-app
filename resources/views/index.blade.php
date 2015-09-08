f1b76d2ecf00c3d056ccbb2fa871864f8fd0c127
Nick Franciosi <nickfranciosi@gmail.com>
2015-09-08 13:54:22 -0500

    moved share scripts to footer

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -271,6 +271,26 @@
     </div>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="/js/libs.js"></script>
-    <script src="/js/app.js"></script>
+    <script src="/js/app.js"></script><script type="text/javascript">
+
+   
+    $(function(){
+        $('#fbTest').on('click',function(){
+            var userImagePath = '';
+            @if(isset($user))
+                userImagePath = '{!! $user->image_path !!}';
+            @endif
+            
+            FB.ui({
+              method: 'share',
+              link: 'https://developers.facebook.com/docs/',
+              caption: 'A new Caption',
+              href: _globalObj._root_url,
+              picture: _globalObj._root_url + userImagePath
+          }, function(response){});
+        });
+    });
+    
+</script>
   </body>
 </html>
