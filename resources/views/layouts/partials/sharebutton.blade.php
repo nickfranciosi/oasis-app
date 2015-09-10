2bbc4e544f6fbe78dd956a3f60232ae21fe3214b
Nick Franciosi <nickfranciosi@gmail.com>
2015-09-10 16:10:33 -0500

    updated share dialog

--- resources/views/layouts/partials/sharebutton.blade.php
+++ resources/views/layouts/partials/sharebutton.blade.php
@@ -1,7 +1,7 @@
 
 @if (isset($user) && Auth::check())
 @if ($user->facebook_user_id == Auth::user()->facebook_user_id)
-<button id="fbTest">Share on Facebook</button><a href="https://www.facebook.com/dialog/share?app_id=1467629810225761&amp;href=http://oasis-app.xyz/&amp;display=popup&amp;picture=http://oasis-app.xyz/generated/10100411290737202-1441556548.png&amp;redirect_uri=http://oasis-app.xyz/">test</a><a href="https://twitter.com/share" class="twitter-share-button" data-text="Oasis Awareness Campaign" data-via="oasis" data-size="large" data-count="none" data-hashtags="oasis">Tweet</a>
+<button id="fbTest">Share on Facebook</button><a href="https://www.facebook.com/dialog/share?app_id=1467629810225761&amp;href=http://oasis-app.xyz/&amp;display=popup&amp;picture=http://oasis-app.xyz/{{$user-&gt;image_path}}&amp;redirect_uri=http://oasis-app.xyz/">test</a><a href="https://twitter.com/share" class="twitter-share-button" data-text="Oasis Awareness Campaign" data-via="oasis" data-size="large" data-count="none" data-hashtags="oasis">Tweet</a>
 <a href="/{{ $user->image_path }}" id="downloadLink" download>Download</a>
 @endif
 @endif
