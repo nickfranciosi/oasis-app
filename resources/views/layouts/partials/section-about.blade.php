b3b9c7ffc6e2bcc6a922083c3e2642ab7b308b9d
Nick Franciosi <nickfranciosi@gmail.com>
2015-09-10 15:00:21 -0500

    updated index links

--- resources/views/layouts/partials/section-about.blade.php
+++ resources/views/layouts/partials/section-about.blade.php
@@ -9,7 +9,7 @@
         <p>The teen years are a difficult time for anyone. Challenges posed by peer pressure, drugs, academic demands and even sexual orientation force many young people to make difficult choices that can complicate their lives even more.</p>
       </div>
       <div class="col-sm-6">
-        <p>Oasis is here to help at-risk youths navigate tricky waters of the teenage years with a wide range of programs that are designed to empower them and make them aware of their potential, turning them into happy, well-adjusted adults.</p><a href="https://www.oasiscenter.org/#about" target="_blank" class="btn-link">Learn More at oasiscenter.org<i class="icon-arrow"></i>
+        <p>Oasis is here to help at-risk youths navigate tricky waters of the teenage years with a wide range of programs that are designed to empower them and make them aware of their potential, turning them into happy, well-adjusted adults.</p><a href="https://www.oasiscenter.org/#programs" target="_blank" class="btn-link">Learn More at oasiscenter.org<i class="icon-arrow"></i>
           <!-- @import ../includes/svg-arrow-right.html--></a>
       </div>
     </div>
--- resources/views/layouts/partials/section-help.blade.php
+++ resources/views/layouts/partials/section-help.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
       <div class="col-sm-offset-6 col-sm-6">
         <h2 class="header">How you can help</h2>
-        <p>You have the power to make a difference in a teen’s life. At Oasis, we value each relationship we have with every supporter and count on you to help us get the job done. To find out how you can donate click on the button below.</p><a href="https://www.oasiscenter.org" target="_blank" class="btn btn-main space-top">Make a Donation</a>
+        <p>You have the power to make a difference in a teen’s life. At Oasis, we value each relationship we have with every supporter and count on you to help us get the job done. To find out how you can donate click on the button below.</p><a href="https://www.oasiscenter.org/donate.html" target="_blank" class="btn btn-main space-top">Make a Donation</a>
       </div>
     </div>
   </div>
