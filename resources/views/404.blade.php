a15b80bafd03309a66c1ef5158a7f319dcdcfac0
Nick Franciosi <nickfranciosi@gmail.com>
2015-09-11 16:05:58 -0500

    updated share dialog to be popup

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -180,7 +180,8 @@
 
    
     $(function(){
-        $('#fbTest').on('click',function(){
+        $('#fb-share').on('click',function(e){
+            e.preventDefault();
             var userImagePath = '';
             var userURL = _globalObj._root_url;
             @if(isset($user))
@@ -188,13 +189,18 @@
                 userURL += 'profile/{!! $user->facebook_user_id !!}';
             @endif
             console.log(_globalObj._root_url + userImagePath);
-            FB.ui({
-              method: 'share',
-              link: userURL,
-              caption: 'Oasis',
-              href: userURL,
-              picture: _globalObj._root_url + userImagePath
-          }, function(response){});
+
+            if( navigator.userAgent.match('CriOS') ){
+              window.open( 'https://www.facebook.com/dialog/share?app_id='+_globalObj._facebook_app_id+'&href=http://oasis-app.xyz/'+userURL+'&display=popup&picture=http://oasis-app.xyz/'+userImagePath+'&redirect_uri=http://oasis-app.xyz/'+userURL," ", "status = 1, height = 500, width = 360, resizable = 0" )
+            }else{
+                FB.ui({
+                  method: 'share',
+                  link: userURL,
+                  caption: 'Oasis',
+                  href: userURL,
+                  picture: _globalObj._root_url + userImagePath
+              }, function(response){});
+            }
         });
     });
 
