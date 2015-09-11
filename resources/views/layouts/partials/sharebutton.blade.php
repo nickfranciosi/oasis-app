ae704db554e2a0ef50082e3a7af57fcf56be362b
Nick Franciosi <nickfranciosi@gmail.com>
2015-09-11 15:59:38 -0500

    updated share dialog to be popup

--- resources/views/layouts/partials/sharebutton.blade.php
+++ resources/views/layouts/partials/sharebutton.blade.php
@@ -1,6 +1,6 @@
 
 @if (isset($user) && Auth::check())
-@if ($user->facebook_user_id == Auth::user()->facebook_user_id)<a href="https://www.facebook.com/dialog/share?app_id=1467629810225761&href=http://oasis-app.xyz/profile/{!! $user->facebook_user_id !!}&display=popup&picture=http://oasis-app.xyz/{!! $user->image_path !!}&redirect_uri=http://oasis-app.xyz/profile/{!! $user->facebook_user_id !!}">Share on Facebook</a>
+@if ($user->facebook_user_id == Auth::user()->facebook_user_id)<a href="#" id="fb-share">Share on Facebook</a>
 <a href="https://twitter.com/share" class="twitter-share-button" data-text="Oasis Awareness Campaign" data-via="oasis" data-size="large" data-count="none" data-hashtags="oasis">Tweet</a>
 <a href="/{{ $user->image_path }}" id="downloadLink" download>Download</a>
 @endif
