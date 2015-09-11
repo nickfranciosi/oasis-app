2365122dc06830c0932c4410816720b4efd43fa3
Nick Franciosi <nickfranciosi@gmail.com>
2015-09-11 11:42:13 -0500

    force steps to final image preview page after fb login

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -191,6 +191,7 @@ $(window).on('load', function() {
 
   if($.cookie('first_word') != undefined){
     console.log('we have a cookie set');
+    $('#picture-steps').steps('next');
     setWordsOnImageFromCookie($.cookie('first_word'), $.cookie('second_word'));
   }
   // Preloader
