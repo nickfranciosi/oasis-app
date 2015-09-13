26b279baea0d46732d26046a254c160777a5e33c
Nick Franciosi <nickfranciosi@gmail.com>
2015-09-13 13:35:02 -0500

    profile share updates

--- resources/views/layouts/partials/end.blade.php
+++ resources/views/layouts/partials/end.blade.php
@@ -4,29 +4,54 @@
 
    
     $(function(){
-        $(document).delegate('#fbTest','click',function(event){
-            event.preventDefault();
-            console.log('fb share');
-            var userImagePath = '';
-            var userURL = _globalObj._root_url;
-            @if(isset($user))
-                userImagePath = '{!! $user->image_path !!}';
-                userURL += 'profile/{!! $user->facebook_user_id !!}';
-            @endif
-            console.log(_globalObj._root_url + userImagePath);
+        $('#fb-profile-share').on('click', function(e){
+          e.preventDefault();
+          console.log('time to share');
 
-            if( navigator.userAgent.match('CriOS') ){
-              window.open( 'https://www.facebook.com/dialog/share?app_id='+_globalObj._facebook_app_id+'&href=http://oasis-app.xyz/'+userURL+'&display=popup&picture=http://oasis-app.xyz/'+userImagePath+'&redirect_uri=http://oasis-app.xyz/'+userURL," ", "status = 1, height = 500, width = 360, resizable = 0" );
-            }else{
-                FB.ui({
-                  method: 'share',
-                  link: userURL,
-                  caption: 'Oasis',
-                  href: userURL,
-                  picture: _globalObj._root_url + userImagePath
-              }, function(response){});
-            }
+          var userImagePath = '';
+          var userURL = _globalObj._root_url;
+          @if(isset($user))
+              userImagePath = '{!! $user->image_path !!}';
+              userURL += 'profile/{!! $user->facebook_user_id !!}';
+          @endif
+          console.log(_globalObj._root_url + userImagePath);
+
+          if( true ){
+            window.open( 'https://www.facebook.com/dialog/share?app_id='+_globalObj._facebook_app_id+'&href=http://oasis-app.xyz/'+userURL+'&display=popup&picture=http://oasis-app.xyz/'+userImagePath+'&redirect_uri=http://oasis-app.xyz/'+userURL," ", "status = 1, height = 500, width = 360, resizable = 0" );
+          }else{
+            FB.ui({
+                method: 'share',
+                link: userURL,
+                caption: 'Oasis',
+                href: userURL,
+                picture: _globalObj._root_url + userImagePath
+            }, function(response){});
+          }
+          
         });
+        // $(document).delegate('#fbTest','click',function(event){
+        //     event.preventDefault();
+        //     console.log('fb share');
+            // var userImagePath = '';
+            // var userURL = _globalObj._root_url;
+            // @if(isset($user))
+            //     userImagePath = '{!! $user->image_path !!}';
+            //     userURL += 'profile/{!! $user->facebook_user_id !!}';
+            // @endif
+            // console.log(_globalObj._root_url + userImagePath);
+
+        //     if( navigator.userAgent.match('CriOS') ){
+        //       window.open( 'https://www.facebook.com/dialog/share?app_id='+_globalObj._facebook_app_id+'&href=http://oasis-app.xyz/'+userURL+'&display=popup&picture=http://oasis-app.xyz/'+userImagePath+'&redirect_uri=http://oasis-app.xyz/'+userURL," ", "status = 1, height = 500, width = 360, resizable = 0" );
+        //     }else{
+        //         FB.ui({
+        //           method: 'share',
+        //           link: userURL,
+        //           caption: 'Oasis',
+        //           href: userURL,
+        //           picture: _globalObj._root_url + userImagePath
+        //       }, function(response){});
+        //     }
+        // });
     });
 
 
