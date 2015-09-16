edbed224c2042bfc86d3a6da31a36b6505c992ef
Eric Motil <emotil@Eric-Motil.local>
2015-09-16 17:09:59 -0500

    trying @unless blade condition

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -146,13 +146,17 @@
               </div>
               <div class="col-sm-7">
                 <h2 class="header">{{ $user->name }}</h2>
-                <p>Oasis Outreach Center encourages people to make a statement against teen stereotypes. Help us by creating or sharing your own customized profile photo.</p>@if (isset($user) && Auth::check())<a href="/picture" class="btn btn-main">Create Your Own Picture</a>@if ($user->facebook_user_id == Auth::user()->facebook_user_id)
+                <p>Oasis Outreach Center encourages people to make a statement against teen stereotypes. Help us by creating or sharing your own customized profile photo.</p>
+                @if (isset($user) && Auth::check())
+                @if ($user->facebook_user_id == Auth::user()->facebook_user_id)
                 <div class="share-block">
                   <h3 class="sub-header">Share Your Photo</h3><a href="https://www.facebook.com/dialog/share?app_id=1467629810225761&href=http://oasis-app.xyz/profile/{!! $user->facebook_user_id !!}&display=popup&picture=http://oasis-app.xyz/{!! $user->image_path !!}&redirect_uri=http://oasis-app.xyz/profile/{!! $user->facebook_user_id !!}" alt="Facebook"><i class="fa fa-facebook"></i></a>
                   <a href="https://twitter.com/share"  data-text="Oasis Awareness Campaign" data-via="oasis" data-size="large" data-count="none" data-hashtags="oasis" alt="Twitter"><i class="fa fa-twitter"></i></a>
                   <a href="/{{ $user->image_path }}" id="downloadLink" download="{{ str_slug($user->name)}}.png" alt="Download"><i class="fa fa-download"></i></a>
                 </div>@endif
                 @endif
+                @unless (Auth::check())
+                <div class="share-block"><a href="/picture" class="btn btn-main">Create Your Own Picture</a></div>@endunless
               </div>
             </div>
           </div>
