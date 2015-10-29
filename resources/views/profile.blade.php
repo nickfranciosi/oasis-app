28f5550a03fe63b754020bfb1792c7dc275407c3
Nick Franciosi <nickfranciosi@gmail.com>
2015-10-29 10:13:21 -0500

    removed app id from share link

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -150,7 +150,7 @@
                 @if (isset($user) && Auth::check())
                 @if ($user->facebook_user_id == Auth::user()->facebook_user_id)
                 <div class="share-block">
-                  <h3 class="sub-header">Share Your Photo</h3><a href="https://www.facebook.com/dialog/share?app_id=1467629810225761&amp;href=http://moretoyouth.com/profile/{!! $user->facebook_user_id !!}&amp;display=popup&amp;picture=http://moretoyouth.com/{!! $user->image_path !!}&amp;redirect_uri=http://moretoyouth.com/profile/{!! $user->facebook_user_id !!}" alt="Facebook"><i class="fa fa-facebook"></i></a>
+                  <h3 class="sub-header">Share Your Photo</h3><a href="https://www.facebook.com/dialog/share?href=http://moretoyouth.com/profile/{!! $user->facebook_user_id !!}&amp;display=popup&amp;picture=http://moretoyouth.com/{!! $user->image_path !!}&amp;redirect_uri=http://moretoyouth.com/profile/{!! $user->facebook_user_id !!}" alt="Facebook"><i class="fa fa-facebook"></i></a>
                   <a href="https://twitter.com/share"  data-text="Oasis Awareness Campaign" data-via="oasis" data-size="large" data-count="none" data-hashtags="oasis" alt="Twitter"><i class="fa fa-twitter"></i></a>
                   <a href="/{{ $user->image_path }}" id="downloadLink" download="{{ str_slug($user->name)}}.png" alt="Download"><i class="fa fa-download"></i></a>
                 </div>@endif
